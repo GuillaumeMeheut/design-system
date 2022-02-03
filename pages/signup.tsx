bc017c96334dd3dd16a1b1cac6f44376ee7151b9
guillaume.meheutpro@gmail.com <xlgktakx@gmail.com>
2022-02-03 15:33:53 +0100

    fix type

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import { AppButton } from '../components'
+import { Color } from '../theme'
 
 const SignUp: NextPage = () => {
     return (
         <div>
-            <AppButton text={`S'inscrire`}></AppButton>
+            <AppButton text={`S'inscrire`} color={Color.primary}></AppButton>
         </div >
     )
 }
