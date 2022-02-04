f668843cf4804ca948806975e051a6843c33017c
guillaume.meheutpro@gmail.com <xlgktakx@gmail.com>
2022-02-04 14:17:13 +0100

    add input

---
+++ components/input/index.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import css from './style.module.scss'
+
+type Props = {
+    label: string,
+    id: string,
+    password: boolean,
+    color: { color: string }
+    fonts: { fontSize: string, fontWeight: string }
+    size: any
+};
+
+export const AppInput = ({ label, id, password, color, fonts, size }: Props) => {
+    console.log({ ...fonts, ...color, ...size });
+
+    const [visible, setVisible] = useState(!password)
+
+    return (
+        <div>
+            <label htmlFor={id}>{label}</label>
+            <input id={id} type={visible ? "text" : 'password'} />
+            {password ? <button onClick={() => setVisible(!visible)}>svg oeil</button> : <></>}
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
-import { AppButton, AppText } from '../components'
+import { AppButton, AppInput, AppText } from '../components'
 import css from '../styles/index.module.scss'
-import { ButtonSize, Color, Typography } from '../theme'
+import { ButtonSize, Color, InputSize, Typography } from '../theme'
 
 const SignIn: NextPage = () => {
     return (
         <div>
             <AppButton text={"Se connecter"} color={Color.primary} fonts={Typography.title} size={ButtonSize.button} />
             <AppText Tag={'h3'} text={"Se connecter"} color={Color.primary} fonts={Typography.title} ></AppText>
+            <AppInput label={"Email"} id={"Email"} password={false} color={Color.danger} fonts={Typography.para} size={InputSize.input} />
         </div>
     )
 }
