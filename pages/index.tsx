0969c2a1abcba32a3a84e0a66fed30d91b3cc6ad
guillaume.meheutpro@gmail.com <xlgktakx@gmail.com>
2022-02-07 10:16:55 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,15 +12,15 @@ const SignUp: NextPage = () => {
             <div className={css.bgContainer}>
                 <div className={css.contentContainer}>
 
-                    <AppText Tag={'h3'} text={"Sign up"} color={Color.black} fonts={Typography.title} />
+                    <AppText Tag={'h3'} text={"Sign up"} color={Color.white} fonts={Typography.title} />
                     <div className={css.textContainer}>
                         <div style={{ marginRight: "5px" }}>
-                            <AppText Tag={'p'} text={"Already got registered?"} color={Color.black} fonts={Typography.para} />
+                            <AppText Tag={'p'} text={"Already got registered?"} color={Color.white} fonts={Typography.para} />
                         </div>
                         <AppText Tag={'a'} href="/signin" text={"Sign in"} color={Color.tertiary} fonts={Typography.para} />
                     </div>
-                    <AppInput label={"Email"} labelColor={Color.black} id={"email"} password={false} color={Color.primary} fonts={Typography.para} size={InputSize.input} />
-                    <AppInput label={"Password"} labelColor={Color.black} id={"password"} password={true} color={Color.primary} fonts={Typography.para} size={InputSize.input} />
+                    <AppInput label={"Email"} labelColor={Color.white} id={"email"} password={false} color={Color.primary} fonts={Typography.para} size={InputSize.input} />
+                    <AppInput label={"Password"} labelColor={Color.white} id={"password"} password={true} color={Color.primary} fonts={Typography.para} size={InputSize.input} />
                     <AppButton text={"Sign Up"} color={Color.white} fonts={Typography.para} size={ButtonSize.button} onClick={() => validation()} />
 
                 </div>
