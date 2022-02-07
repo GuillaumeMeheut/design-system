4ddfa5f1d2a8a93db9ca9ba6c4602dca8fb31c6e
guillaume.meheutpro@gmail.com <xlgktakx@gmail.com>
2022-02-07 10:27:01 +0100

    fix forgot password

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -13,17 +13,17 @@ const ForgotPassword: NextPage = () => {
         <div className={css.page}>
             <div className={css.bgContainer}>
                 <div className={css.contentContainer}>
-
                     <AppText Tag={'h3'} text={"Forgot password"} color={Color.white} fonts={Typography.title} />
                     <div className={css.textContainer}>
-                        <div style={{ marginRight: "5px" }}>
+                        <span style={{ marginRight: "5px" }}>
                             <AppText Tag={'p'} text={"Return to "} color={Color.white} fonts={Typography.para} />
-                        </div>
+                        </span>
                         <AppText Tag={'a'} href="/signin" text={"Sign In"} color={Color.tertiary} fonts={Typography.para} />
                     </div>
-                    <AppInput label={"Password"} labelColor={Color.white} id={"password"} password={true} color={Color.primary} fonts={Typography.para} size={InputSize.input} />
+                    <div className={css.input}>
+                        <AppInput label={"Password"} labelColor={Color.white} id={"password"} password={true} color={Color.primary} fonts={Typography.para} size={InputSize.input} />
+                    </div>
                     <AppButton text={"Send"} color={Color.white} fonts={Typography.para} size={ButtonSize.button} onClick={() => validation()} />
-
                 </div>
             </div>
         </div>
